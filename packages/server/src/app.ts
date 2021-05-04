19e9d896d7ba98144b39dfdcdcf0a08085c533e6
lukekeum <lukekeum11@gmail.com>
2021-05-05 00:28:11 +0900

    feat(server): Setup jwt with express-jwt

--- packages/server/src/app.ts
+++ packages/server/src/app.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import { ApolloServer } from 'apollo-server-express';
 import generateSchema from './graphql';
+import jwt from 'express-jwt';
 
 class App {
   private _app: express.Application;
@@ -9,12 +10,30 @@ class App {
     this._app = express();
 
     this._apolloServerSetup();
+
+    this._app.use(
+      '/graphql',
+      jwt({
+        secret: String(process.env.JWT_SECRET),
+        credentialsRequired: false,
+        algorithms: [],
+      })
+    );
   }
 
   private async _apolloServerSetup() {
     const schema = await generateSchema();
 
-    const server = new ApolloServer({ schema });
+    const server = new ApolloServer({
+      schema,
+      context: ({ req }) => {
+        const context = {
+          req,
+          user: req.user,
+        };
+        return context;
+      },
+    });
 
     server.applyMiddleware({ app: this._app });
 
